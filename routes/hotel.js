07696ab23fc4d420c8a387200d78a0341c9020f5
Shantanu Milkhe <102807647+shantanumilkhe@users.noreply.github.com>
2022-11-05 13:51:45 +0530

    UI Modification

--- routes/hotel.js
+++ routes/hotel.js
@@ -7,8 +7,16 @@ const hotel = require('../controllers/hotel')
 router.route('/')
     .get(catchAsync(hotel.index))
 
-router.get('/booking', catchAsync( async (req, res, next) =>{
+router.get('/booking/:id', catchAsync( async (req, res, next) =>{
     res.render('campgrounds/booking');
+}))
+
+
+
+router.post('/postBooking', catchAsync( async (req, res, next) =>{
+console.log(req.body.book);
+const redirecturl = "/hotel/booking"
+res.redirect(redirecturl);
 }))
 
 module.exports = router;
