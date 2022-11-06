dfb69909bcb29a0ec04ab863a2e5de7b4d888ff3
nikhilnaik5 <82027742+nikhilnaik5@users.noreply.github.com>
2022-11-06 12:27:41 +0530

    Fixed routes

--- routes/hotel.js
+++ routes/hotel.js
@@ -7,10 +7,21 @@ const catchAsync = require("../Utility/catchAsync");
 const hotel = require('../controllers/hotel')
 
 router.route('/')
-    .get(catchAsync(hotel.index));
+    .get(catchAsync(hotel.index))
 
 router.route('/v2')
     .get(catchAsync(hotel.indexv2));
 
+router.get('/booking/:id', catchAsync( async (req, res, next) =>{
+    res.render('campgrounds/booking');
+}))
+
+
+
+router.post('/postBooking', catchAsync( async (req, res, next) =>{
+console.log(req.body.book);
+const redirecturl = "/hotel/booking"
+res.redirect(redirecturl);
+}))
 
 module.exports = router;
