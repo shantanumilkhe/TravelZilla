4c84c498ec7c75ac3f606d725be8dc26a122b378
nikhilnaik5 <82027742+nikhilnaik5@users.noreply.github.com>
2022-11-06 12:23:42 +0530

    Added Hotel listing

--- routes/hotel.js
+++ routes/hotel.js
@@ -1,22 +1,16 @@
-// import fetch from "node-fetch";
+// // import fetch from "node-fetch";
 const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
 const router = express.Router({mergeParams: true});
 const catchAsync = require("../Utility/catchAsync");
 const hotel = require('../controllers/hotel')
 
 router.route('/')
-    .get(catchAsync(hotel.index))
+    .get(catchAsync(hotel.index));
 
-router.get('/booking/:id', catchAsync( async (req, res, next) =>{
-    res.render('campgrounds/booking');
-}))
+router.route('/v2')
+    .get(catchAsync(hotel.indexv2));
 
-
-
-router.post('/postBooking', catchAsync( async (req, res, next) =>{
-console.log(req.body.book);
-const redirecturl = "/hotel/booking"
-res.redirect(redirecturl);
-}))
 
 module.exports = router;
