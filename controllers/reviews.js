bc523f02504b3a4649f226c1cf14033b1743e8aa
nikhilnaik5 <82027742+nikhilnaik5@users.noreply.github.com>
2022-11-09 18:39:50 +0530

    Fixed reviews

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,9 +1,9 @@
-const bnb = require('../models/bnb');
+const Bnb = require('../models/bnb');
 const Review = require('../models/review')
 
 module.exports.createReviews = async (req, res) => {
     const { id } = req.params;
-    const bnb = await bnb.findById(id);
+    const bnb = await Bnb.findById(id);
     const review = new Review(req.body.review);
     review.author = req.user._id;
     bnb.reviews.push(review);
@@ -16,7 +16,7 @@ module.exports.createReviews = async (req, res) => {
 
 module.exports.deleteReviews = async (req, res, next) => {
     const { id, reviewId } = req.params;
-    await bnb.findByIdAndUpdate(id, { $pull: {reviews: reviewId}})
+    await Bnb.findByIdAndUpdate(id, { $pull: {reviews: reviewId}})
     await Review.findByIdAndDelete(reviewId);
     req.flash('success', `Successfully Deleted review`)
     const redirecturl = req.session.returnto || `/bnbs/${id}`;
